b4e0bf592fc1a5f3a89a57654f01d19baadadf86
OctaconDeveloper <kaakadev@gmail.com>
2020-03-12 14:28:25 +0100

    build UI controller

--- js/js.js
+++ js/js.js
@@ -40,7 +40,6 @@ const budgetController = (function() {
 				id = 0;
 			}
 
-
 			//Check transaction type
 			switch(type){
 				case 'exp':
@@ -74,6 +73,8 @@ const UIController = (function(){
 		amount: '.add__value',
 		description: '.add__description',
 		button: '.add__btn',
+		incomeDiv:'.income__list',
+		expenseDiv: '.expenses__list',
 	}
 	return {
 		//Get input from UI and make it public
@@ -83,6 +84,29 @@ const UIController = (function(){
 				amount: document.querySelector(DOMValue.amount).value,
 				description: document.querySelector(DOMValue.description).value,
 			}
+
+		},
+
+		//Add Items to UI
+		addListItem: function(object, type){
+			let html, newHtml, element;
+			//Create HTML string with placeholder text for individual ttpe
+			if(type === 'inc'){
+				element = DOMValue.incomeDiv;
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%amount%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}else{
+				element = DOMValue.expenseDiv;
+				html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%amount%</div><div class="item__percentage">%percentage%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			//Replce placeholder with actual object
+			newHtml = html.replace('%id%', object.id);
+			newHtml = newHtml.replace('%description%', object.description);
+			newHtml = newHtml.replace('%amount%', object.amount);
+
+			
+			// Insert the HTML into tht DOM
+			document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
 
 		},
 		//Return DOM Strings as public values
@@ -119,10 +143,10 @@ const Interface   = (function(budgetCtrl, UICtrl){
 		// console.log(input);
 		
 		//2. Add the tem to the budget controller
-		Item = budgetCtrl.addItem(input.type, input.description, input.amount);
+		newItem = budgetCtrl.addItem(input.type, input.description, input.amount);
 		
 		//3. Add item to the UI
-
+		UICtrl.addListItem(newItem, input.type);
 		
 		//4. Calculate the budget
 
